9c3ad3f4bead99d1c608f296bcd4b9df516921e0
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-02-13 22:58:41 +0500

    Foydalanuvchi yaratish va olish funksiyalari qo'shildi

--- handlers/start.py
+++ handlers/start.py
@@ -5,18 +5,26 @@ from aiogram.dispatcher import FSMContext
 # kode import
 from loader import dp, bot
 from utils import texts, buttons
-from utils.env import ADMIN
-
+from services.services import getUser, createUser
 
 
 @dp.message_handler(commands=['start'], state='*')
 async def start_handler(message: Message, state: FSMContext):
     
     user_id = message.from_user.id
+    user = getUser(user_id)
     
-    if user_id == ADMIN:
-        await message.answer(texts.START_ADMIN)
-    else:
+    if user:
         await message.answer(texts.START)
+    else:
+        name = message.from_user.first_name
+        createuser = {
+            'name': name,
+            'user_id': user_id
+        }
+        createUser(createuser)
         
+        await message.answer(texts.START)
+
+    
                 
--- services/services.py
+++ services/services.py
@@ -1,1 +1,26 @@
 import requests
+from utils.env import BASE_URL
+
+
+
+
+def createUser(user):
+    url = f"{BASE_URL}/users/"
+    response = requests.post(url, json=user)
+    
+    if response.status_code == 200 or 201:
+        data = response.json()
+        return data
+    else:
+        print(response.status_code)
+        
+        
+def getUser(user_id):
+    url = f"{BASE_URL}/users/{user_id}/"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print(response.status_code)
