f793bfae82ff93e9b932cac929d9a719ffd77b73
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-02-13 14:46:37 +0500

    ADMIN o'zgaruvchisini ro'yxatga joylash va yangi video havolalar qo'shildi

--- handlers/start.py
+++ handlers/start.py
@@ -5,15 +5,17 @@ from aiogram.dispatcher import FSMContext
 # kode import
 from loader import dp, bot
 from utils import texts, buttons
-from utils.env import ADMIN
+from utils.env import ADMIN, BOT_ADMIN
 
+
+ADMINS = [ADMIN, BOT_ADMIN]
 
 @dp.message_handler(commands=['start'], state='*')
 async def start_handler(message: Message, state: FSMContext):
     
     user_id = message.from_user.id
     
-    if user_id == ADMIN:
+    if user_id in ADMINS:
         await message.answer(texts.START_ADMIN)
     else:
         await message.answer(texts.START)
