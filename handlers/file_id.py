0d6869f760ee949670caebc15a8d3918fd8cb8b3
Husanboyazamov <azamovhusanboy08@gmail.com>
2025-03-11 14:32:18 +0500

    Adminlar uchun video ID ni yuborish funksiyasi qo'shildi

--- handlers/file_id.py
+++ handlers/file_id.py
@@ -1,14 +1,16 @@
 from aiogram.types import Message
 from aiogram.dispatcher import FSMContext
-
 from loader import dp
 
 
+
+ADMINS= [5765144405,1229568290,7691756537]
 
 
 
 @dp.message_handler(content_types=['video'], state='*')
 async def video_handler(message: Message, state: FSMContext):
-    video_id = message.video.file_id
     
-    await message.answer(video_id)
+    if message.from_user.id in ADMINS:
+        video_id = message.video.file_id
+        await message.answer(video_id)
